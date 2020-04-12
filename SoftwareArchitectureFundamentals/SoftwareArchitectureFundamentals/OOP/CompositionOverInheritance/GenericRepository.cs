dbb45b9c8a83f0b59b934472929057b63edbb632
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-12 12:02:52 -0300

    [ADD] Added GenericRepository class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/OOP/CompositionOverInheritance/GenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareArchitectureFundamentals.OOP.CompositionOverInheritance
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+        where TEntity : class, new()
+    {
+        public void Add(TEntity entity)
+        {
+           
+        }
+
+        public void Remove(TEntity entity)
+        {
+           
+        }
+    }
+}
