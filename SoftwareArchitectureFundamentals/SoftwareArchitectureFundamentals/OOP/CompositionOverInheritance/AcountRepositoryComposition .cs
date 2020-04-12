4fb45cf024d2ee9827cf6e6e975c2efcf343c472
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-12 12:41:37 -0300

    [ADD] Added AcountRepositoryComposition class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/OOP/CompositionOverInheritance/AcountRepositoryComposition .cs
@@ -0,0 +1,16 @@
+﻿namespace SoftwareArchitectureFundamentals.OOP.CompositionOverInheritance
+{
+    public class AcountRepositoryComposition: IAcountRepository
+    {
+        private readonly IGenericRepository<Acount> _repository;
+        public AcountRepositoryComposition(IGenericRepository<Acount> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(Acount acount)
+        {
+            _repository.Add(acount);
+        }
+    }
+}
