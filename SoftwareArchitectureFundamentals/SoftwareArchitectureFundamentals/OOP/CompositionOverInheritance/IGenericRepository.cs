43a0df25f60639cd8f4b51701151a693e778ede8
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-12 12:01:40 -0300

    [ADD] Added IGenericRepository interface

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/OOP/CompositionOverInheritance/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareArchitectureFundamentals.OOP.CompositionOverInheritance
+{
+    public interface IGenericRepository<TEntity>
+        where TEntity: class, new ()
+    {
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
