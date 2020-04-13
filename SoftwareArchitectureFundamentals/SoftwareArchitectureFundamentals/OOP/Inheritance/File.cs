4c55015858b033703b0beef013b906967476c057
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-13 17:02:48 -0300

    [ADD] Added File class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/OOP/Inheritance/File.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace SoftwareArchitectureFundamentals.OOP.Inheritance
+{
+    public abstract class File
+    {
+        protected readonly string _content;
+
+        public File(string content)
+        {
+            _content = content;
+          
+        }
+        public abstract void Save(string path);
+        public abstract void Save(string path, Encoding encoding);
+    }
+}
