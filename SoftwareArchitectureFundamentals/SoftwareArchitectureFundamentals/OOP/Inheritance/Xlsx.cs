be901fea7d9d3448a2c788ad56b329f9182a524f
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-13 17:04:54 -0300

    [ADD] Added Xlsx class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/OOP/Inheritance/Xlsx.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace SoftwareArchitectureFundamentals.OOP.Inheritance
+{
+    public class Xlsx: File
+    {
+        public Xlsx(string content)
+           : base(content)
+        {
+
+        }
+
+        public override void Save(string path)
+        {
+            Debug.WriteLine($"Saving Xlsx file> {_content}");
+        }
+
+        public override void Save(string path, Encoding encoding)
+        {
+            Debug.WriteLine($"Saving Xlsx file> {_content}");
+        }
+    }
+}
