96908653cee83ad6c8fc6be87d61f387bc98dda8
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-13 17:04:11 -0300

    [ADD] Added Txt class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/OOP/Inheritance/Txt.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace SoftwareArchitectureFundamentals.OOP.Inheritance
+{
+    public class Txt : File
+    {
+        public Txt(string content)
+            : base(content)
+        {
+
+        }
+
+        public override void Save(string path)
+        {
+            Debug.WriteLine($"Saving Txt file> {_content}");
+
+        }
+
+        public override void Save(string path, Encoding encoding)
+        {
+            Debug.WriteLine($"Saving Txt file> {_content}");
+        }
+    }
+}
