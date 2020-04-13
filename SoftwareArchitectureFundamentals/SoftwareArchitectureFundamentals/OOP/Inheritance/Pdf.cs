7fe2c729d32581869cfcd31a4803c74f2fbaccc3
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-13 17:03:30 -0300

    [ADD] Added Pdf class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/OOP/Inheritance/Pdf.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace SoftwareArchitectureFundamentals.OOP.Inheritance
+{
+    public class Pdf: File
+    {
+        public Pdf(string content)
+           : base(content)
+        {
+        }
+
+        public override void Save(string path)
+        {
+            Debug.WriteLine($"Saving Pdf file> {_content}");
+        }
+
+        public override void Save(string path, Encoding encoding)
+        {
+            Debug.WriteLine($"Saving Pdf file> {_content}");
+        }
+    }
+}
