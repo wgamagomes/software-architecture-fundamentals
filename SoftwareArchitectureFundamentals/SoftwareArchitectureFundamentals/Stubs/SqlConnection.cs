60b452bd001049cda87f8e25dae3ab9d4988feb1
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-14 14:41:03 -0300

    [ADD] Added SqlConnection (stub class)

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/Stubs/SqlConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SoftwareArchitectureFundamentals.Stubs
+{
+    internal class SqlConnection : IDisposable
+    {
+        public string ConnectionString { get; internal set; }
+
+        public void Dispose()
+        {
+           
+        }
+
+        internal void Open()
+        {
+           
+        }
+    }
+}
