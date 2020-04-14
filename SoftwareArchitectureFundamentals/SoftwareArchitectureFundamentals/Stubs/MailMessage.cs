fbc7989a0c281f5bbee42dd1b11f898d99764010
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-14 14:37:10 -0300

    [ADD] MailMessage (stub class)

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/Stubs/MailMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftwareArchitectureFundamentals.Stubs
+{
+    internal class MailMessage
+    {
+        public MailMessage(string from, string to)
+        {
+            
+        }
+
+        public string Subject { get; internal set; }
+        public string Body { get; internal set; }
+    }
+}
