14ebc6285eee8acd1f4062de072dcee94c63c5cb
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-14 14:38:02 -0300

    [ADD] Added SmtpClient (stub class)

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/Stubs/SmtpClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoftwareArchitectureFundamentals.Stubs
+{
+    internal class SmtpClient
+    {
+        public int Port { get; set; }
+        public object DeliveryMethod { get; set; }
+        public bool UseDefaultCredentials { get; set; }
+        public string Host { get; set; }
+
+        internal void Send(MailMessage mail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
