cef935be7289fd2ba7ff9bc5784be1ac7fe7dcf5
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-14 14:40:25 -0300

    [ADD] Added SqlCommand (stub class)

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/Stubs/SqlCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+
+namespace SoftwareArchitectureFundamentals.Stubs
+{
+    internal class SqlCommand: IDisposable
+    {
+        public SqlCommand()
+        {
+        }
+
+        public SqlConnection Connection { get; internal set; }
+        public CommandType CommandType { get; internal set; }
+        public string CommandText { get; internal set; }
+
+        internal void Parameters(string key, object value)
+        {
+            
+        }
+
+        internal void ExecuteNonQuery()
+        {
+           
+        }
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
