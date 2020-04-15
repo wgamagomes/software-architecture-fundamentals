1123cc90e94fefffb04d7515c60df69fc3cdd894
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-15 16:36:40 -0300

    [ADD] Added MoneyMarketAccount and  SavingsAccount classes

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/OCP/NonViolation/WithInheritance/SavingsAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.OCP.NonViolation.WithInheritance
+{
+    public class SavingsAccount : Account
+    {
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount * .97;
+        }
+    }
+}
