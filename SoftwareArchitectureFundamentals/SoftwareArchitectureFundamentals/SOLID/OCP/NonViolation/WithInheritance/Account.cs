3bce86faf7981e1494a7f76571414879f55e473a
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-15 16:33:30 -0300

    [ADD] Added Account class (OCP NonViolation WithInheritance)

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/OCP/NonViolation/WithInheritance/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.OCP.NonViolation.WithInheritance
+{
+    public abstract class Account
+    {        
+        protected double Balance { get; set; }
+        public string Owner { get; set; }
+        public abstract void Withdraw(double amount);
+    }
+}
