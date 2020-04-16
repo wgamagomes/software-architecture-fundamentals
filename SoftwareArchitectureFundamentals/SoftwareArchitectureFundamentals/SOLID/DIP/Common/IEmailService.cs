67a663a33af05ebc5a4800bc749aa7880c059323
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-16 12:52:31 -0300

    [ADD] Added IEmailService interface

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/DIP/Common/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.DIP.Common
+{
+    public interface IEmailService
+    {
+        void Send(string from, string to, string subject, string body);
+        bool IsValid(string emailAddress);
+    }
+}
