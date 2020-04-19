4dd60ef453afb967d3f5a4f1cabc9b47cd689712
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-19 20:51:43 -0300

    [ALT] Alter dir

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/2. OCP/NonViolation/WithExtensionMethod/BasicAccountWithdraw.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.OCP.NonViolation.WithExtensionMethod
+{
+    public static class BasicAccountWithdraw
+    {
+        public static void Withdraw(this Account account, double amount)
+        {
+            account.Withdraw(balance => balance - amount);
+        }
+    }
+}
---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/2. OCP/NonViolation/WithExtensionMethod/SavingsAccountWithdraw.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.OCP.NonViolation.WithExtensionMethod
+{
+    public static class SavingsAccountWithdraw 
+    {
+        public static void Withdraw(this Account account, double amount)
+        {
+            account.Withdraw(balance => balance - amount * .97);
+        }
+    }
+}
