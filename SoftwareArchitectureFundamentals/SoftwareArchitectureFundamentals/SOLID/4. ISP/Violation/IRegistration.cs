53552015cf5aecabd5414cd0b4c7e214b1629096
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-19 20:55:22 -0300

    [ALT] Alter dir

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/Violation/IRegistration.cs
@@ -0,0 +1,9 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID._4._ISP.Violation
+{
+    public interface IRegistration
+    {
+        void IsValid();
+        void Add();
+        void SendEmail();
+    }
+}
