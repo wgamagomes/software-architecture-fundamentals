88847625c4a11263604cc9038a1dda821f1f758f
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-19 20:52:45 -0300

    [ALT] Alter dir

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/5. DIP/Common/EmailService.cs
@@ -0,0 +1,31 @@
+﻿
+using SoftwareArchitectureFundamentals.Stubs;
+using System.Text.RegularExpressions;
+
+namespace SoftwareArchitectureFundamentals.SOLID.DIP.Common
+{
+    internal class EmailService: IEmailService
+    {
+        public void Send(string from, string to, string subject, string body)
+        {
+            var mailMessage = new MailMessage(from, to);
+
+            var smtpClient = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = "Network",
+                UseDefaultCredentials = false,
+                Host = "smtp.google.com"
+            };
+
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
+            smtpClient.Send(mailMessage);
+        }
+
+        public  bool IsValid(string emailAddress)
+        {
+            return Regex.IsMatch(emailAddress, @"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$");
+        }
+    }
+}
