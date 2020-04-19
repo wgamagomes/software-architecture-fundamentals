9fadea2fb76a6b8dbe9ad19ca5dd508206d853ff
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-19 20:52:12 -0300

    [ALT] Alter dir

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/2. OCP/NonViolation/WithInheritance/MoneyMarketAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.OCP.NonViolation.WithInheritance
+{
+    public class MoneyMarketAccount : Account
+    {
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount * .99;
+        }
+    }
+}
