705699f83dade2a22deb39189a4e17752e0b0cbe
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-20 13:58:18 -0300

    [ADD] Added ProductRegistration class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/Violation/Registration/ProductRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SoftwareArchitectureFundamentals.SOLID.ISP.Violation.Registration
+{
+    class ProductRegistration : IRegistration
+    {
+        public void Add()
+        {
+            // Add new data to a database
+        }
+
+        public void IsValid()
+        {
+            // Validate Data
+        }
+
+        /// <summary>
+        /// Interface Segregation principle
+        /// Clients should not be forced to depend upon interfaces that they do not use
+        /// </summary>           
+        public void SendEmail()
+        {
+            // Product has no email, what do I do now ???
+            throw new NotImplementedException();
+        }
+    }
+}
