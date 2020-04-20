b0a2b28a24dc3a7c23c57ef9423e47f0b07c9d13
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-20 13:59:52 -0300

    [ADD] Added ICustomerRegistration interface

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/Registration/ICustomerRegistration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.Registration
+{
+    public interface ICustomerRegistration: IRegistration
+    {
+        void SendEmail();
+    }
+}
