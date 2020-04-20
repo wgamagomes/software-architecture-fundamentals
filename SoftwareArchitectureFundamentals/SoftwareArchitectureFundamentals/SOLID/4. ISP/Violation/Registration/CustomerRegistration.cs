e10a02f5503506faad77939d55fd41a51ff4432c
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-20 13:57:30 -0300

    [ADD] Added CustomerRegistration class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/Violation/Registration/CustomerRegistration.cs
@@ -0,0 +1,20 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.Violation.Registration
+{
+    class CustomerRegistration : IRegistration
+    {
+        public void Add()
+        {
+            // Add new data to a database
+        }
+
+        public void IsValid()
+        {
+           // Validate Data
+        }
+
+        public void SendEmail()
+        {
+            //Send a email to customer
+        }
+    }
+}
