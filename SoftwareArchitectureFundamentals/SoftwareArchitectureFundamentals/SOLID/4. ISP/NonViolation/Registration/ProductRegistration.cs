fdc26e7245e803ff11a372b89188e5adb6642545
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-20 14:01:53 -0300

    [ADD] Added ProductRegistration class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/Registration/ProductRegistration.cs
@@ -0,0 +1,15 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.Registration
+{
+    class ProductRegistration : IProductRegistration
+    {
+        public void Add()
+        {
+            // Add new data to a database
+        }
+
+        public void IsValid()
+        {
+            // Validate Data
+        }
+    }
+}
