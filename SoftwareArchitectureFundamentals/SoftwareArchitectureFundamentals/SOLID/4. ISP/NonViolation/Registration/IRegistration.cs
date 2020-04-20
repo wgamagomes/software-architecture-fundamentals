75b3a01ea2bdb4dd8e668d3d7f04fd1161162ba2
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-20 13:59:12 -0300

    [ADD] Added IRegistration interface

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/Registration/IRegistration.cs
@@ -0,0 +1,8 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.Registration
+{
+    public interface IRegistration
+    {
+        void IsValid();
+        void Add();       
+    }
+}
