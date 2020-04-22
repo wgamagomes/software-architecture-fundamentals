27a2441a5dcb9a1e1d794e6d0d3190f9f3bef85b
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-22 08:48:27 -0300

    [ADD] Added EspressoGroundCoffee class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/Violation/CoffeeMachine/EspressoGroundCoffee.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.Violation.CoffeeMachine
+{
+
+    public class EspressoGroundCoffee : IGroundCoffee
+    {
+        public EspressoGroundCoffee(double quantity)
+        {
+            QuantityGroundCoffee = quantity;
+        }
+        public double QuantityGroundCoffee { get; private set; }
+        public CoffeeType CoffeeType => CoffeeType.Basic;
+
+        public void SetQuantity(double quantity)
+        {
+            QuantityGroundCoffee = quantity;
+        }
+    }
+}
