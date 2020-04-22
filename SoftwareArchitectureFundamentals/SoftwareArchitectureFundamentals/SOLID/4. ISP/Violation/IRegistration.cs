8f346723dea96c83955958c34a5bbaae6e11587e
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-21 21:23:58 -0300

    [DEL] IRegistration

--- SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/Violation/IRegistration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SoftwareArchitectureFundamentals.SOLID._4._ISP.Violation
-{
-    public interface IRegistration
-    {
-        void IsValid();
-        void Add();
-        void SendEmail();
-    }
-}
