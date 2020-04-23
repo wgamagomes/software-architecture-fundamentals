42ee08acc352098066a6aed9d2ab2fd9fcd1f5ac
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-23 20:08:09 -0300

    [ADD] Added IBrewingUnit interface

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/CoffeeMachine/Interfaces/IBrewingUnit.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.CoffeeMachine
+{
+    public interface IBrewingUnit
+    {
+        CoffeeDrink Brew(IGroundCoffee groundCoffee, double quantityWater);
+    }
+}
