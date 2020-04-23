6525b80610cad02102ee8ac9527e60efd9316395
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-23 20:09:26 -0300

    [ADD] Added CoffeeDrink class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/CoffeeMachine/CoffeeDrink.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.CoffeeMachine
+{
+    public class CoffeeDrink
+    {
+        public CoffeeDrink(IGroundCoffee groundCoffee, double quantityWater)
+        {
+            CoffeeType = groundCoffee.CoffeeType;
+
+            QuantityWater = quantityWater;
+
+            QuantityGroundCoffee = groundCoffee.QuantityGroundCoffee;
+        }
+
+        public CoffeeType CoffeeType { get; }
+        public double QuantityWater { get; }
+        public double QuantityGroundCoffee { get; }
+    }
+}
