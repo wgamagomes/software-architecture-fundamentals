72091b74f9bd15582121f436cdc568cb2e79ac26
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-25 10:00:51 -0300

    [ADD] Added CoffeeException class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/CoffeeMachine/CoffeeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.CoffeeMachine
+{
+    public class CoffeeException: Exception
+    {
+        public CoffeeException(string message) :
+            base(message)
+        {
+
+        }
+    }
+}
