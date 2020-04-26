25e4a840020497b32a802a1951f558f22f21f24b
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-26 16:55:41 -0300

    [ADD] Added EspressoCoffeeMachine class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/CoffeeMachine/EspressoCoffeeMachine.cs
@@ -0,0 +1,31 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.CoffeeMachine
+{
+    public class EspressoCoffeeMachine : IEspressoCoffeeMachine
+    {
+        private readonly IBrewingUnit _BrewingUnit;
+        private readonly IGroundCoffee _groundCoffee;
+        private readonly double _quantityWater;
+
+        public EspressoCoffeeMachine(IBrewingUnit brewingUnit , double quantityGroundCoffee, double quantityWater)
+        {
+            _BrewingUnit = brewingUnit;
+            _groundCoffee = new EspressoGroundCoffee(quantityGroundCoffee);
+            _quantityWater = quantityWater;
+        }
+         
+
+        public CoffeeDrink BrewEspresso()
+        {
+            return _BrewingUnit.Brew(_groundCoffee, _quantityWater);
+        }
+
+        /// <summary>
+        /// Interface Segregation principle
+        /// Clients should not be forced to depend upon interfaces that they do not use
+        /// </summary>        
+        public CoffeeDrink BrewFilterCoffee()
+        {
+            throw new CoffeeException("This machine only brew espresso.");
+        }
+    }
+}
