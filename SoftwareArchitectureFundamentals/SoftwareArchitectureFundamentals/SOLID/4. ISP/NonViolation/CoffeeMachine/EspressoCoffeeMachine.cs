af30613df95ff9538c9434f7eb4dbcd8bbcd7488
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-26 17:03:41 -0300

    [ADD] Added EspressoCoffeeMachine class

---
+++ SoftwareArchitectureFundamentals/SoftwareArchitectureFundamentals/SOLID/4. ISP/NonViolation/CoffeeMachine/EspressoCoffeeMachine.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftwareArchitectureFundamentals.SOLID.ISP.NonViolation.CoffeeMachine
+{
+    public class EspressoCoffeeMachine : IEspressoCoffeeMachine
+    {
+        private readonly IBrewingUnit _BrewingUnit;
+        private readonly IGroundCoffee _groundCoffee;
+        private readonly double _quantityWater;
+
+        public EspressoCoffeeMachine(IBrewingUnit brewingUnit , double quantityGroundCoffee, double quantityWater)
+        {
+            _BrewingUnit = brewingUnit;
+            _groundCoffee = new EspressoGroundCoffee(quantityGroundCoffee);
+            _quantityWater = quantityWater;
+        }
+         
+
+        public CoffeeDrink BrewEspresso()
+        {
+            return _BrewingUnit.Brew(_groundCoffee, _quantityWater);
+        }
+    }
+}
